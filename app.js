565547472015cabe846fec63a3779fd44bb550a0
Shravan Kumar <40226836+shravan341@users.noreply.github.com>
2024-07-18 09:49:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
-import ToDoScreen from "./Screens/ToDoScreen";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AddTask from "./components/AddTask";
+const http=require('http');
 
-const router = createBrowserRouter([
-    {
-        path: "/", // Homepage
-        element: <ToDoScreen />,
-        errorElement: <h1>404 - Not Found</h1> // Improved error message
-    }, 
+const server=http.createServer((req,res)=>{
+  res.write("This is some response from your first nodejs server");
+  res.end();
+})
 
-    {
-    path:"/add-task",
-    element: <AddTask/>
-    }
-]);
-
-const App = () => {
-    const [tasks, setTasks] =  useState([]);;
-    // return<ToDoScreen/>
-    return <RouterProvider router={router} />;
-};
-
-export default App;
+server.listen(5000,()=>{
+  console.log("server has started")
+})
